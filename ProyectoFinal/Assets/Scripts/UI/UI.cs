85c40cd708eab10df9facbb132aea9875a20cec3
Enochinator44 <ignacio_garcia_m-ts3dv1mc2122@cev.com>
2023-01-16 14:34:33 +0100

    Menu

---
+++ ProyectoFinal/Assets/Scripts/UI/UI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject mainScene, start, options, exit;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
